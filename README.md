fb51551ff0096b9891c90db8748a32728e646fdd
manisharan <manisharanreddyvancha@gmail.com>
2026-05-15 16:31:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# NetVerse
+
+Realtime Multiplayer Internet Infrastructure Simulator
+
+## Tech Stack
+
+- React
+- Node.js
+- Express
+- MariaDB
+- Sequelize
+- Socket.IO
+- Docker
---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const sequelize = require('./database')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "NetVerse API running"
+    })
+})
+
+async function startServer() {
+    try {
+        await sequelize.authenticate()
+        console.log('Database connected successfully!')
+
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`)
+        })
+    } catch(err) {
+        console.error('Database connection failed: ', err)
+    }
+}
+
+startServer()
